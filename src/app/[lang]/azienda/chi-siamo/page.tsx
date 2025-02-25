68a4db6f54b554d4c3602813f5a00ff78dabbfaf
leonegithub <webmaster@leone.it>
2025-02-25 10:21:26 +0100

    feat: chi-siamo page

---
+++ src/app/[lang]/azienda/chi-siamo/page.tsx
@@ -0,0 +1,65 @@
+import { getDictionary } from "../../dictionaries";
+import Image from "next/image";
+import fotoSito from "./Foto Sito 20246.png";
+import "./style.css";
+
+const ChiSiamo = async ({
+  params,
+}: {
+  params: Promise<{ lang: "it" | "en" }>;
+}) => {
+  const lang = (await params).lang;
+  const dict = await getDictionary(lang);
+
+  const descriptions = [
+    {
+      title: dict.chi_siamo.description_1.title,
+      text: dict.chi_siamo.description_1.text,
+    },
+    {
+      title: dict.chi_siamo.description_2.title,
+      text: dict.chi_siamo.description_2.text,
+    },
+    {
+      title: dict.chi_siamo.description_3.title,
+      text: dict.chi_siamo.description_3.text,
+    },
+    {
+      title: dict.chi_siamo.description_4.title,
+      text: dict.chi_siamo.description_4.text,
+    },
+  ];
+
+  return (
+    <div>
+      <div className="img-container">
+        <Image className="leone-logo" src={fotoSito} alt="leone-grande" />
+      </div>
+
+      <div className="chi-siamo container pt-4">
+        {descriptions.map((description, index) => (
+          <Description
+            key={index}
+            title={description.title}
+            text={description.text}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Description = ({ title, text }: { title: string; text: string }) => {
+  const paragraphs = text.split(" || ");
+
+  return (
+    <div className="description">
+      <h1 className="font-bold blue py-3">{title}</h1>
+      {paragraphs.map((paragraph, index) => (
+        <p key={index} dangerouslySetInnerHTML={{ __html: paragraph }} />
+      ))}
+    </div>
+  );
+};
+
+export default ChiSiamo;
