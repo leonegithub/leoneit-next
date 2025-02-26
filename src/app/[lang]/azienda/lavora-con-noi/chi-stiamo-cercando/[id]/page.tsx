73e593e3ccef3226a51ddfb7aabea5d9d82ce71d
leonegithub <webmaster@leone.it>
2025-02-26 12:00:24 +0100

    feat: [id] page

---
+++ src/app/[lang]/azienda/lavora-con-noi/chi-stiamo-cercando/[id]/page.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import parse from "html-react-parser";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import Button from "@/components/button";
+
+export default function PositionDetail() {
+  const params = useParams();
+  const id = params.id as string;
+  const router = useRouter();
+
+  const [position, setPosition] = useState<Position | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  interface Position {
+    id: string;
+    titolo: string;
+    descrizione: string;
+  }
+
+  const handleApplication = (id: string) => {
+    router.push(`/it/azienda/lavora-con-noi/invia-candidatura?id=${id}`);
+  };
+
+  useEffect(() => {
+    if (id) {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            "https://php.leone.it/api/GetPosizioni.php?lingua=IT&stato=aperto",
+            {
+              headers: {
+                Authorization:
+                  "Bearer wlfca9P8Zn0zQt4zwpcDne4KJROqEOAzIy3dr0Eyxhbzhqz4ydddgjc",
+              },
+            }
+          );
+          const data = await response.json();
+          console.log(data.ReturnedObject);
+          if (data && data.ReturnedObject) {
+            const position: Position | undefined = data.ReturnedObject.find(
+              (item: { id: string }) => item.id.toString() === id
+            );
+            setPosition(position || null);
+          } else {
+            console.log("Errore: la struttura dei dati non è corretta");
+          }
+        } catch (error) {
+          console.log("Errore durante la chiamata API: ", error);
+          setError((error as any).message);
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchData();
+    }
+  }, [id]);
+
+  return (
+    <div className="container mx-auto p-4">
+      {loading ? (
+        <div className="flex justify-center items-center">
+          <div className="spinner-border text-primary" role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      ) : error ? (
+        <p className="text-red-500">Errore: {error}</p>
+      ) : (
+        <div>
+          <h1 className="text-2xl font-bold">{position?.titolo}</h1>
+          <div className="prose">{parse(position?.descrizione || "")}</div>
+          <span onClick={() => position && handleApplication(position.id)}>
+            <Button testo="Candidati" />
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
