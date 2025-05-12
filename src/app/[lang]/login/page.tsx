adb543eadccc26f9d0235f15517c276a6e08991e
webmaster-leoneit <webmaster@leone.it>
2025-05-12 11:59:41 +0200

    fix: register and login page

--- src/app/[lang]/login/page.tsx
+++ src/app/[lang]/login/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import LoginForm from "@/components/loginForm";
 import { useRouter, useParams } from "next/navigation";
 
-export function getCookie(name: string): string | null {
+function getCookie(name: string): string | null {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
   if (parts.length === 2) return parts.pop()?.split(";").shift() || null;
