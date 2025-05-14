8a6abb2797edcebb971c6d585f6638e959493275
webmaster-leoneit <webmaster@leone.it>
2025-05-14 14:20:03 +0200

    fix: promise instead of lang

--- src/app/[lang]/register/page.tsx
+++ src/app/[lang]/register/page.tsx
@@ -1,7 +1,8 @@
 import { getDictionary } from "../dictionaries";
 import RegisterFormClient from "./RegisterFormClient";
 
-export default async function RegisterPage({ params }: { params: { lang: "it" | "en" } }) {
-  const dict = await getDictionary(params.lang);
-  return <RegisterFormClient dict={dict} lang={params.lang} />;
+export default async function RegisterPage({ params }: { params: Promise<{ lang: "it" | "en" }> }) {
+  const {lang} = await params;
+  const dict = await getDictionary(lang);
+  return <RegisterFormClient dict={dict} lang={lang} />;
 }
