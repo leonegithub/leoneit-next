2d0f218125f80dd3794820bf174b2851dd74c7ce
webmaster-leoneit <webmaster@leone.it>
2025-05-15 14:40:58 +0200

    fix: i beat chat

--- src/app/[lang]/distributori/worldwide/page.tsx
+++ src/app/[lang]/distributori/worldwide/page.tsx
@@ -2,8 +2,8 @@ import { redirect } from "next/navigation";
 import { getDictionary } from "../../dictionaries";
 import WorldwideClient from "./WorldwideClient";
 
-export default async function WorldwidePage({ params }: { params: { lang: "it" | "en" } }) {
-  const { lang } = params;
+export default async function WorldwidePage({ params }: { params: Promise<{ lang: "it" | "en"}> }) {
+  const { lang } = await params;
   const dict = await getDictionary(lang);
 
   if (lang === "it") {
