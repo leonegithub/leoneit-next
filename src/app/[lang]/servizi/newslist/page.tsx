cdcaea0d1673e333054c14d4ef5648f1c20277a8
webmaster-leoneit <webmaster@leone.it>
2025-05-29 11:54:40 +0200

    feat: newslist page

--- src/app/[lang]/servizi/newslist/page.tsx
+++ src/app/[lang]/servizi/newslist/page.tsx
@@ -2,7 +2,7 @@ import { getDictionary } from "../../dictionaries";
 import NewslistClient from "./NewslistClient";
 
 export default async function NewslistPage({ params }: { params: { lang: "it" | "en" } }) {
-  const { lang } = params;
+  const { lang } = await params;
   const dict = await getDictionary(lang);
 
   return <NewslistClient lang={lang} dict={dict} />;
