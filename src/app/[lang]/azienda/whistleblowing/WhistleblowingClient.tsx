d86b44834d1f4872da549e55209c616acab87781
webmaster-leoneit <webmaster@leone.it>
2025-06-04 11:22:16 +0200

    fix: userouter client only

---
+++ src/app/[lang]/azienda/whistleblowing/WhistleblowingClient.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function WhistleblowingClient({ lang }: { lang: "it" | "en" }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (lang !== "it") {
+      router.push("/");
+    }
+  }, [lang, router]);
+
+  if (lang !== "it") return null;
+
+  return (
+    <div className="container jumbo">
+      <h1 className="blue font-bold pt-4">Whistleblowing</h1>
+       <p className="my-4">
+        La normativa sul whistleblowing si è evoluta negli ultimi anni,
+        soprattutto in risposta alle esigenze di una maggiore responsabilità
+        sociale e trasparenza delle aziende. Leone S.P.A. in adeguamento alla
+        normativa D.Lgs. 24/2023, si è dotata di una piattaforma per le
+        segnalazioni, sia in forma scritta sia orale, in modalità anche anonima.
+        Nella sezione “Policy per le segnalazioni” sono disponibili tutte le
+        informazioni necessarie per effettuare una segnalazione in relazione
+        alle casistiche oggetto della normativa, la gestione della segnalazione,
+        le tutele del segnalante e del segnalato, nonché ogni altra informazione
+        utile in ordine alla procedura. Nelle “Linee guida” per il segnalante
+        sono riepilogate tutte le indicazioni per procedere ad effettuare una
+        segnalazione.
+        <br /> Le segnalazioni potranno essere inviate direttamente nella{" "}
+        <a
+          href="https://whistleblowersoftware.com/secure/9b2942ba-709e-4492-8230-b19feb8fddb7"
+          target="__blank"
+          className="blue underline"
+        >
+          piattaforma informatica Whistleblowing
+        </a>
+      </p>
+      <div className="policy-procedures">
+        <ul>
+          <li>
+            <a
+              href="https://leone.it/servizi/download/Whistleblowing-Policy_gestione_segnalazioni.pdf"
+              target="__blank"
+              className="blue underline"
+            >
+              Policy per la gestione delle segnalazioni
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://leone.it/servizi/download/Whistleblowing-Procedura-gestione-segnalazioni_01.pdf"
+              target="__blank"
+              className="blue underline"
+            >
+              Procedura per la gestione delle segnalazioni
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
