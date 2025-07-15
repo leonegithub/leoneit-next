221bc603d5a7c90c644e8a110a951e7a4adb66fb
webmaster-leoneit <webmaster@leone.it>
2025-07-15 15:22:11 +0200

    feat: broken buttons on cart

--- src/components/cart-components/AddButton.tsx
+++ src/components/cart-components/AddButton.tsx
@@ -11,7 +11,7 @@ const AddButton: React.FC<AddButtonProps> = ({ onAdd, disabled }) => {
       disabled={disabled}
       type="button"
       onClick={onAdd}
-      className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      className="text-white bg-blue bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
     >
       Add to cart
     </button>
